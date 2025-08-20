daea53c0d9d7c477630b8e498df33aae71e7a4aa
PetCuraApp <petcurasocialmedia@gmail.com>
2025-08-20 19:27:33 -0400

    desplegar 3

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -160,7 +160,7 @@ export default function Contact() {
                 width="100%"
                 height="100%"
                 style={{ border: 0 }}
-                allowFullScreen=""
+                allowFullScreen={true}
                 loading="lazy"
                 referrerPolicy="no-referrer-when-downgrade"
               ></iframe>
